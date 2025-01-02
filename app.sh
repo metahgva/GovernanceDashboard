820bee3fb5ea8ff5303799baffc3f298ea1ff1b4
Ahmet Gyger <ahmet.gyger@dominodatalab.com>
2025-01-02 07:15:44 -0800

    Pushed from Domino: https://se-demo.domino.tech/workspace/ahmet_gyger/GovernanceDashboard?executionId=67449ddc191e8f19a5d10b9b

--- app.sh
+++ app.sh
@@ -1,3 +1,5 @@
+pip install streamlit --user
+
 mkdir ~/.streamlit
 echo "[browser]" > ~/.streamlit/config.toml
 echo "gatherUsageStats = true" >> ~/.streamlit/config.toml
@@ -7,5 +9,5 @@ echo "[server]" >> ~/.streamlit/config.toml
 echo "port = 8888" >> ~/.streamlit/config.toml
 echo "enableCORS = false" >> ~/.streamlit/config.toml
 echo "enableXsrfProtection = false" >> ~/.streamlit/config.toml
-
+ 
 streamlit run app.py
