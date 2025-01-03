38fc2eb68c2cbd37af3a76f977802bc7c6a6945d
Ahmet Gyger <ahmet.gyger@dominodatalab.com>
2025-01-03 10:18:14 -0800

    Pushed from Domino: https://se-demo.domino.tech/workspace/ahmet_gyger/GovernanceDashboard?executionId=67449ddc191e8f19a5d10b9b

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 import os
 
+
 # Load API Host and Key from environment variables or fallback values
 API_HOST = os.getenv("API_HOST", "https://se-demo.domino.tech")
 API_KEY = os.getenv("API_KEY", "2627b46253dfea3a329b8c5b84748b98d5b3c5ffe6eb02a55f7177231fc8c1c4")
