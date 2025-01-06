3af4336a4d3862e67e6676bcd52c4dec50b9a1f8
Ahmet Gyger <ahmet.gyger@dominodatalab.com>
2025-01-06 05:16:06 -0800

    Pushed from Domino: https://se-demo.domino.tech/workspace/ahmet_gyger/GovernanceDashboard?executionId=67449ddc191e8f19a5d10b9b

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import re
 #   ENV CONFIG & CONSTANTS
 # ----------------------------------------------------
 API_HOST = os.getenv("API_HOST", "https://se-demo.domino.tech")
-API_KEY = os.getenv("API_KEY", "YOUR_REAL_API_KEY_HERE")  # or your masked value
+API_KEY = os.getenv("API_KEY", "2627b46253dfea3a329b8c5b84748b98d5b3c5ffe6eb02a55f7177231fc8c1c4") 
 
 st.title("Deliverables and Projects Dashboard")
 
