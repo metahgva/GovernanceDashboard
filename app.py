0f6e4b171dc1aab333b012a4a1f72f612b9cc91a
Ahmet Gyger <ahmet.gyger@dominodatalab.com>
2025-04-02 10:26:21 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/ahmet_gyger/GovernanceDashboard?executionId=67449ddc191e8f19a5d10b9b

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ st.markdown(
 API_HOST = os.getenv("API_HOST", "https://se-demo.domino.tech")
 API_KEY = os.getenv("API_KEY", "2627b46253dfea3a329b8c5b84748b98d5b3c5ffe6eb02a55f7177231fc8c1c4")
 
-st.title("Bundles and Projects Dashboard")
+st.title("Governance Dashboard")
 
 # ----------------------------------------------------
 #   SIDEBAR NAVIGATION
